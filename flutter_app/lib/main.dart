39bb0791c159a95e2f2f486378d0f945a2707ccc
besumero628 <besumero628@gmail.com>
2023-01-31 22:47:20 +0900

    scaffoldとappbarの設定

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,25 +6,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter'),
+        ),
+        body: const Text(
+          'Hello Flutter World!!',
+          style: TextStyle(fontSize: 32.0),
+        ),
+
+      )
     );
   }
 }
@@ -58,7 +57,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      print("HelloWorld");
     });
   }
 
