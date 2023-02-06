bb0994f66e8e0f17bf397c18b41fb314e53d878a
besumero628 <besumero628@gmail.com>
2023-02-06 22:20:36 +0900

    flitter studioの利用

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,72 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  final title = 'Flutterサンプル';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(
-        title: this.title,
-      )
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title}): super();
-  final String title;
-
+  MyHomePage({Key? key}) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class Data {
-  int _price;
-  String _name;
-  Data(this._name, this._price): super();
-
-  @override
-  String toString() {
-    return _name + ':' + _price.toString() + '円';
-  }
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static final _data = [
-    Data('Apple', 200),
-    Data('Orange', 150),
-    Data('Peach', 300)
-  ];
-  Data _item = _data[0];
-
-  void _setData() {
-    setState(() {
-      _item = (_data..shuffle()).first;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Set data'),
-      ),
-      body: Text(
-        _item.toString(),
-        style: TextStyle(fontSize: 32.0),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setData,
-        tooltip: 'set message',
-        child: Icon(Icons.star),
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('App Name'),
+          ),
+        body:
+          new Text(
+          "Hello Flutter!",
+            style: new TextStyle(fontSize:32.0,
+            color: const Color(0xFF000000),
+            fontWeight: FontWeight.w700,
+            fontFamily: "Roboto"),
+          ),
+    
+      );
+    }
 }
