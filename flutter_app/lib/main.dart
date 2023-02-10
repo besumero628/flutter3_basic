7d6658df49f7deb53c2f3f40df88261b4a80fe19
besumero628 <besumero628@gmail.com>
2023-02-10 21:17:48 +0900

    bottom navigationの使用

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -28,68 +28,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static var _message = 'ok';
-  static var _stars = '☆☆☆☆☆';
-  static var _star = 0;
+  static var _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My App'),
-        leading: BackButton(
-          color: Colors.white,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.android),
-            tooltip: 'add star...',
-            onPressed: iconPressdA,
-          ),
-          IconButton(
-            icon: Icon(Icons.favorite),
-            tooltip: 'subtract star...',
-            onPressed: iconPressdB,
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(30.0),
-          child: Text(
-            _stars,
-            style: TextStyle(
-              fontSize: 22.0,
-              color: Colors.white,
-            ),
-          ),
-        ),
       ),
       body: Center(
         child: Text(
           _message,
           style: const TextStyle(
-            fontSize: 28.0,
+            fontSize: 28.0
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        backgroundColor: Colors.lightBlueAccent,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Android',
+            icon: Icon(
+              Icons.android,
+              color: Colors.black,
+              size: 50,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Favorite',
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 50,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+              size: 50,
+            ),
+          ),
+        ],
+        onTap: tapBottomIcon,
       ),
     );
   }
 
-  void iconPressdA() {
-    _message = 'tap "android".';
-    _star++;
-    update();
-  }
-
-  void iconPressdB() {
-    _message = 'tap "favorite".';
-    _star--;
-    update();
-  }
-
-  void update() {
-    _star = _star < 0 ? 0 : _star > 5 ? 5 : _star;
+  void tapBottomIcon(int value) {
+    var items = ['Android', 'Heart', 'Home'];
     setState(() {
-      _stars = '★★★★★☆☆☆☆☆'.substring(5 - _star, 5 - _star + 5);
-      _message = _message + '[$_star]';
+      _index = value;
+      _message = 'you tapped: "' + items[_index] + '".';
     });
   }
 }
