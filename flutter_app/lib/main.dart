7ef65acb200eb8e05fcd27cb7561367482e52237
besumero628 <besumero628@gmail.com>
2023-03-25 11:10:06 +0900

    Spriteの使用

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/services.dart';
+import 'package:flame/components.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,44 +42,32 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class SampleGame extends FlameGame with KeyboardEvents {
-  late final paint;
-  late Vector2 _loc;
+
+  @override
+  Color backgroundColor() => const Color(0xffCCCCFF);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    paint = Paint();
-    paint.color = Colors.blue;
-    _loc = Vector2(100, 100);
+    add(MySprite(Vector2(100, 100)));
+  }
+}
+
+class MySprite extends SpriteComponent {
+  late final Vector2 _position;
+
+  MySprite(this._position): super();
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await Sprite.load('chara.png');
+    position = _position;
+    size = Vector2(100, 100);
   }
 
   @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    final rect = Rect.fromLTWH(_loc.x, _loc.y, 100, 100);
-    canvas.drawOval(rect, paint);
-  }
-
-  @override
-  KeyEventResult onKeyEvent(
-    RawKeyEvent event,
-    Set<LogicalKeyboardKey> keysPressed,
-  ) {
-    final _dpos = Vector2(0, 0);
-
-    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-      _dpos.x = -10;
-    }
-    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-      _dpos.x = 10;
-    }
-    if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
-      _dpos.y = -10;
-    }
-    if (keysPressed.contains(LogicalKeyboardKey.arrowDown)) {
-      _dpos.y = 10;
-    }
-    _loc += _dpos;
-    return KeyEventResult.handled;
+  void update(double delta) {
+    super.update(delta);
   }
 }
